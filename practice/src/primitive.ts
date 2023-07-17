42d044380d50bcdc449f7c148ac3e3096aac5e5e
kazuhiro nagamine <kazoopi6@gmail.com>
2023-07-17 22:52:49 +0900

    2-3-2, 2-3-5
    number型（2進数、8進数、16進数サンプル）
    テンプレートリテラル ``(バッククォート)

---
+++ practice/src/primitive.ts
@@ -0,0 +1,18 @@
+const num :number = 10;
+
+console.log(`num : ${num}`);
+
+// 2進数
+const binary : number = 0b1010;
+
+// 8進数
+const octa :number = 0o740;
+
+// 16進数
+const hexa : number = 0x1ABC;
+
+console.log(`binary : ${binary}`);
+
+console.log(`octa : ${octa}`);
+
+console.log(`hexa : ${hexa}`);
